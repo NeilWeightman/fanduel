b4adbeed120e9c3ceec2690bc303904d39d253f8
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2021-08-24 16:58:23 +0100

    Add files via upload

---
+++ Inheritance-Solution/ShoppingCart.java
@@ -0,0 +1,9 @@
+package com.fanduel.inheritance;
+
+public class ShoppingCart {
+    public static void main(String args[]){ 
+	// Instantiate a Shirt object and call display() on the object reference
+        Shirt shirt = new Shirt(25.99, 'M', 'P');
+        shirt.display();       
+    }
+}    
